3dea8069520ddb0c15c01dd282c5a5b7dbe278ea
piero123XD <101850205+piero123XD@users.noreply.github.com>
2023-11-03 17:17:09 -0500

    cliente.js

--- cliente.js
+++ cliente.js
@@ -1,22 +1,40 @@
+var socket = new WebSocket("ws://localhost:8770");
+var mensajes = [];
+
+socket.onopen = function(event) {
+    console.log("Conexión WebSocket abierta");
+};
+
+socket.onmessage = function(event) {
+    console.log("Mensaje recibido del servidor: " + event.data);
+};
+
+socket.onclose = function(event) {
+    if (event.wasClean) {
+        console.log("Conexión WebSocket cerrada de manera limpia, código=" + event.code);
+    } else {
+        console.error("Conexión WebSocket cerrada de manera inesperada");
+    }
+};
+
+socket.onerror = function(error) {
+    console.error("Error en la conexión WebSocket: " + error.message);
+};
+
+// Definir el orden deseado de las IDs de los campos
+var order = ['mensaje0', 'mensaje1', 'mensaje2', 'mensaje3', 'mensaje4', 'mensaje5', 'mensaje6', 'mensaje7', 'mensaje8', 'mensaje9', 'mensaje10', 'mensaje11'];
+
 // Agregar un controlador de eventos "blur" a los campos de texto y correo electrónico
 var textAndEmailElements = document.querySelectorAll('input[type="text"], input[type="email"]');
-textAndEmailElements.forEach(function(inputElement, index) {
+textAndEmailElements.forEach(function(inputElement) {
     inputElement.addEventListener('blur', function(event) {
-        if (index === 0 || index === textAndEmailElements.length - 1) {
-            // Si es el primer o último input, llenar todos los demás con espacios en blanco
-            textAndEmailElements.forEach(function(input, i) {
-                if (i !== index) {
-                    input.value = ''; // Establecer el valor en blanco
-                }
-            });
-        }
         enviarMensaje(inputElement.id, inputElement.value);
     });
 });
 
 // Agregar un controlador de eventos "change" a los campos de opción de radio
 var radioElements = document.querySelectorAll('input[type="radio"]');
-radioElements.forEach(function(radioElement, index) {
+radioElements.forEach(function(radioElement) {
     radioElement.addEventListener('change', function(event) {
         enviarMensaje(radioElement.id, radioElement.value);
     });
@@ -25,7 +43,7 @@ radioElements.forEach(function(radioElement, index) {
 function enviarMensaje(id, value) {
     var index = order.indexOf(id);
     if (index !== -1) {
-        mensajes[index] = value;
+        mensajes[index] = value; // Almacena solo el valor en el arreglo en la posición correcta
         var valores = mensajes.filter(function (mensaje) {
             return mensaje !== undefined;
         });
