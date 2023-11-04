b44b783322803a27fb4668644c6280eb77e4492f
piero123XD <101850205+piero123XD@users.noreply.github.com>
2023-11-03 22:49:13 -0500

    cliente.js

--- cliente.js
+++ cliente.js
@@ -1,12 +1,31 @@
-// Crear un objeto para almacenar los valores de los elementos
-var valores = {};
+var socket = new WebSocket("ws://localhost:8770");
+var mensajes = {};
 
-// Agregar un controlador de eventos "blur" a los campos de texto y correo electrónico
+socket.onopen = function(event) {
+    console.log("Conexión WebSocket abierta");
+};
+
+socket.onmessage = function(event) {
+    console.log("Mensaje recibido del servidor: " + event.data);
+};
+
+socket.onclose = function(event) {
+    if (event.wasClean) {
+        console.log("Conexión WebSocket cerrada de manera limpia, código=" + event.code);
+    } else {
+        console.error("Conexión WebSocket cerrada de manera inesperada");
+    }
+};
+
+socket.onerror = function(error) {
+    console.error("Error en la conexión WebSocket: " + error.message);
+};
+
+// Agregar un controlador de eventos "input" a los campos de texto y correo electrónico
 var textAndEmailElements = document.querySelectorAll('input[type="text"], input[type="email"]');
 textAndEmailElements.forEach(function(inputElement) {
-    inputElement.addEventListener('blur', function(event) {
-        valores[inputElement.id] = inputElement.value;
-        enviarMensaje(valores);
+    inputElement.addEventListener('input', function(event) {
+        enviarMensaje(inputElement);
     });
 });
 
@@ -14,17 +33,12 @@ textAndEmailElements.forEach(function(inputElement) {
 var radioElements = document.querySelectorAll('input[type="radio"]');
 radioElements.forEach(function(radioElement) {
     radioElement.addEventListener('change', function(event) {
-        valores[radioElement.id] = radioElement.value;
-        enviarMensaje(valores);
+        enviarMensaje(radioElement);
     });
 });
 
-function enviarMensaje(valores) {
-    // Obtener los valores como un array en el orden deseado según sus IDs
-    var inputElements = document.querySelectorAll('input[type="text"], input[type="email"], input[type="radio"]');
-    var mensajes = Array.from(inputElements).map(function(inputElement) {
-        return valores[inputElement.id] || '';
-    });
-
-    socket.send(JSON.stringify(mensajes));
+function enviarMensaje(inputElement) {
+    mensajes[inputElement.id] = inputElement.value;
+    var valores = Object.values(mensajes);
+    socket.send(JSON.stringify(valores));
 }
