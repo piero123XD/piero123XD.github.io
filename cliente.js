138976aa42c0b815508005d673dee99d7e998f3b
piero123XD <101850205+piero123XD@users.noreply.github.com>
2023-11-03 20:06:34 -0500

    cliente.js

--- cliente.js
+++ cliente.js
@@ -22,30 +22,22 @@ socket.onerror = function(error) {
 };
 
 // Agregar un controlador de eventos "blur" a los campos de texto y correo electrónico
-var elementosConIndices = {};
-
+var textAndEmailElements = document.querySelectorAll('input[type="text"], input[type="email"]');
 textAndEmailElements.forEach(function(inputElement, index) {
-    elementosConIndices[inputElement] = index;
     inputElement.addEventListener('blur', function(event) {
-        enviarMensaje(inputElement);
+        enviarMensaje(inputElement, index);
     });
 });
 
+// Agregar un controlador de eventos "change" a los campos de opción de radio
+var radioElements = document.querySelectorAll('input[type="radio"]');
 radioElements.forEach(function(radioElement, index) {
-    elementosConIndices[radioElement] = index;
     radioElement.addEventListener('change', function(event) {
-        enviarMensaje(radioElement);
+        enviarMensaje(radioElement, index);
     });
 });
 
-function enviarMensaje(inputElement) {
-    var index = elementosConIndices[inputElement];
-    
-    if (index !== undefined) {
-        mensajes[index] = inputElement.value;
-        socket.send(JSON.stringify(mensajes));
-    }
+function enviarMensaje(inputElement, index) {
+    mensajes[index] = inputElement.value;
+    socket.send(JSON.stringify(mensajes));
 }
-
-
-
