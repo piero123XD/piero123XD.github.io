88e3c039435f3db9d1c5a2c62670658f2ad2dddc
piero123XD <101850205+piero123XD@users.noreply.github.com>
2023-11-03 23:04:09 -0500

    cliente.js

--- cliente.js
+++ cliente.js
@@ -1,8 +1,25 @@
-// Definir un array de IDs en el orden deseado
-var idsEnOrden = ["mensaje1", "mensaje2", "mensaje3", "mensaje4", "mensaje5"];
-
-// Crear un objeto para almacenar los valores de los elementos
+var socket = new WebSocket("ws://localhost:8770");
 var valores = {};
+
+socket.onopen = function(event) {
+    console.log("Conexión WebSocket abierta");
+};
+
+socket.onmessage = function(event) {
+    console.log("Mensaje recibido del servidor: " + event.data);
+};
+
+socket.onclose = function(event) {
+    if (event.wasClean) {
+        console.log("Conexión WebSocket cerrada de manera limpia, código=" + event.code);
+    } else {
+        console.error("Conexión WebSocket cerrada de manera inesperada");
+    }
+};
+
+socket.onerror = function(error) {
+    console.error("Error en la conexión WebSocket: " + error.message);
+};
 
 // Agregar un controlador de eventos "input" a los campos de texto y correo electrónico
 var textAndEmailElements = document.querySelectorAll('input[type="text"], input[type="email"]');
@@ -23,10 +40,9 @@ radioElements.forEach(function(radioElement) {
 });
 
 function enviarMensaje(valores) {
-    // Crear un array con los valores en el orden deseado según las IDs
+    var idsEnOrden = Object.keys(valores); // Obtener las IDs en el orden en que se han almacenado
     var valoresEnOrden = idsEnOrden.map(function(id) {
-        return valores[id] || '';
+        return valores[id];
     });
-
     socket.send(JSON.stringify(valoresEnOrden));
 }
